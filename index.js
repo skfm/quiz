ea2708609bd65f488f2d733ac64fed15f7138cdc
SEIYA <skkasans@gmail.com>
2020-07-19 20:44:51 +0900

    正誤判定機能を実装

--- index.js
+++ index.js
@@ -9,7 +9,41 @@ const correct = 'はじめしゃちょー';
 
 document.getElementById('js-question').textContent = question;
 
-document.getElementsByTagName('button')[0].textContent = answers[0];
-document.getElementsByTagName('button')[1].textContent = answers[1];
-document.getElementsByTagName('button')[2].textContent = answers[2];
-document.getElementsByTagName('button')[3].textContent = answers[3];
+const $button = document.getElementsByTagName('button');
+
+$button[0].textContent = answers[0];
+$button[1].textContent = answers[1];
+$button[2].textContent = answers[2];
+$button[3].textContent = answers[3];
+
+$button[0].addEventListener('click', () => {
+  if (correct === $button[0].textContent) {
+    window.alert('正解');
+  } else {
+    window.alert('不正解');
+  }
+});
+
+$button[1].addEventListener('click', () => {
+  if (correct === $button[1].textContent) {
+    window.alert('正解');
+  } else {
+    window.alert('不正解');
+  }
+});
+
+$button[2].addEventListener('click', () => {
+  if (correct === $button[2].textContent) {
+    window.alert('正解');
+  } else {
+    window.alert('不正解');
+  }
+});
+
+$button[3].addEventListener('click', () => {
+  if (correct === $button[3].textContent) {
+    window.alert('正解');
+  } else {
+    window.alert('不正解');
+  }
+});
