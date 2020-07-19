e4c1e74a25301ae3ff394e788751ab60524c5336
SEIYA <skkasans@gmail.com>
2020-07-19 21:01:55 +0900

    コードのリファクタリング

--- index.js
+++ index.js
@@ -7,43 +7,30 @@ const answers = [
 ];
 const correct = 'はじめしゃちょー';
 
-document.getElementById('js-question').textContent = question;
-
 const $button = document.getElementsByTagName('button');
+const buttonLength = $button.length;
 
-$button[0].textContent = answers[0];
-$button[1].textContent = answers[1];
-$button[2].textContent = answers[2];
-$button[3].textContent = answers[3];
+const setupQuiz = () => {
+  document.getElementById('js-question').textContent = question;
+  for(let i = 0; i < buttonLength; i++) {
+    $button[i].textContent = answers[i];
+  }
+}
+setupQuiz();
 
-$button[0].addEventListener('click', () => {
-  if (correct === $button[0].textContent) {
+const clickHandler = (e) => {
+  if (correct === e.target.textContent) {
     window.alert('正解');
   } else {
     window.alert('不正解');
   }
-});
+}
 
-$button[1].addEventListener('click', () => {
-  if (correct === $button[1].textContent) {
-    window.alert('正解');
-  } else {
-    window.alert('不正解');
-  }
-});
+for (let i = 0; i < buttonLength; i++) {
+  $button[i].addEventListener('click', (e) => {
+    clickHandler(e);
+  });
+}
 
-$button[2].addEventListener('click', () => {
-  if (correct === $button[2].textContent) {
-    window.alert('正解');
-  } else {
-    window.alert('不正解');
-  }
-});
 
-$button[3].addEventListener('click', () => {
-  if (correct === $button[3].textContent) {
-    window.alert('正解');
-  } else {
-    window.alert('不正解');
-  }
-});
+
