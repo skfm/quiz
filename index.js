4a460077e9253d868fed24a621ad8578ca076e4a
SEIYA <skkasans@gmail.com>
2020-07-19 21:34:02 +0900

    複数の問題を答えられるように実装

--- index.js
+++ index.js
@@ -1,28 +1,68 @@
 const question = '2020年7月、最も登録者数の多いユーチューバーは？';
-const answers = [
-  'はじめしゃちょー',
-  'Hikakin TV',
-  '東海オンエア',
-  'ヒカル(Hikaru)'
+const quiz = [
+  {
+    question: '2020年7月、最も登録者数が多いユーチューバーは？',  
+    answers: [
+      'はじめしゃちょー',
+      'Hikakin TV',
+      'ヒカル(Hikaru)',
+      '東海オンエア'
+    ],
+    correct: 'はじめしゃちょー'
+  },
+
+  {
+    question: '東海オンエアは何人組のユーチューバーか？',  
+    answers: [
+      '2人',
+      '4人',
+      '6人',
+      '8人'
+    ],
+    correct: '6人'
+  },
+
+  {
+    question: '江頭2:50のYouTube登録者数は約何人か',  
+    answers: [
+      '100万人',
+      '150万人',
+      '200万人',
+      '250万人'
+    ],
+    correct: '200万人'
+  },
 ];
-const correct = 'はじめしゃちょー';
+
+const quizLength = quiz.length;
+let quizIndex = 0;
+let score = 0;
 
 const $button = document.getElementsByTagName('button');
 const buttonLength = $button.length;
 
 const setupQuiz = () => {
-  document.getElementById('js-question').textContent = question;
+  document.getElementById('js-question').textContent = quiz[quizIndex].question;
   for(let i = 0; i < buttonLength; i++) {
-    $button[i].textContent = answers[i];
+    $button[i].textContent = quiz[quizIndex].answers[i];
   }
 }
 setupQuiz();
 
 const clickHandler = (e) => {
-  if (correct === e.target.textContent) {
+  if (quiz[quizIndex].correct === e.target.textContent) {
     window.alert('正解');
+    score++;
   } else {
     window.alert('不正解');
+  }
+
+  quizIndex++;
+
+  if(quizIndex < quizLength) {
+    setupQuiz();
+  } else {
+    window.alert('終了!あなたの正解数は' + score + '/' + quizLength + 'です!');
   }
 }
 
