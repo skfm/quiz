4fc6c61976e16fbe04a3baf1d4616425ec1c4d04
SEIYA <skkasans@gmail.com>
2020-07-19 20:25:19 +0900

    1つの問題文と選択肢を表示

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const question = '2020年7月、最も登録者数の多いユーチューバーは？';
+const answers = [
+  'はじめしゃちょー',
+  'Hikakin TV',
+  '東海オンエア',
+  'ヒカル(Hikaru)'
+];
+const correct = 'はじめしゃちょー';
 
+document.getElementById('js-question').textContent = question;
 
+document.getElementsByTagName('button')[0].textContent = answers[0];
+document.getElementsByTagName('button')[1].textContent = answers[1];
+document.getElementsByTagName('button')[2].textContent = answers[2];
+document.getElementsByTagName('button')[3].textContent = answers[3];
